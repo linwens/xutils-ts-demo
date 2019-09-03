ace7def0117deb86ecc9620bfdfaac59c9a29e71
linwens_xdf <linwenshu@xdf.cn>
2019-09-03 15:57:45 +0800

    build(tsconfig): 打包es5,

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,3 @@
+import Greeter from './greeter';
 import ArrayEach from './arrayEach';
-declare const _default: {
-    Greeter: (name: string) => string;
-    ArrayEach: typeof ArrayEach;
-};
-export default _default;
+export { Greeter, ArrayEach };
